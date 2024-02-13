65d72091bd84b246b57df2896a6d333b299bcb00
James Robinson <160552r@acadiau.ca>
2024-02-13 19:09:11 -0400

    Added Forced Screen Rotation
    When entering the battle scene, the screen is forced to landscape orientation.
    
    When entering the list scene, the screen is forced to portrait orientation.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,11 +8,13 @@ public class SceneLoader : MonoBehaviour
     public void LoadBattleScene()
     {
         SceneManager.LoadScene("BattleScene");
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 
     public void LoadMainScene()
     {
         SceneManager.LoadScene("ListScene");
+        Screen.orientation = ScreenOrientation.Portrait;
     }
 }
 
