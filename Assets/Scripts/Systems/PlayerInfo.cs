c12007f7f382c22097a0f2a2c9d6b4f7e039c6a1
James Robinson <160552r@acadiau.ca>
2024-02-25 11:32:29 -0400

    New Task Button Functional

---
+++ Assets/Scripts/Systems/PlayerInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public static int playerLevel;
+    public static int exp;
+
+}
--- Assets/Scripts/Systems/TaskManager.cs
+++ Assets/Scripts/Systems/TaskManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class TaskManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject taskPrefab;
+    [SerializeField] private GameObject taskContainer;
+    [SerializeField] private Vector3 topOfList;
+    [SerializeField] private float taskGap = 200.0f;
+    public void CreateNewTask()
     {
-        
-    }
+        //Get Transform of lowest task in list
+        Transform lastTaskTransform = null;
+        foreach (Transform task in taskContainer.transform)
+        {
+            lastTaskTransform = task;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject newTask = Instantiate(taskPrefab, new Vector3(0,0,0), Quaternion.identity);
+        newTask.transform.SetParent(taskContainer.transform);
+        newTask.transform.localScale = new Vector3(1,1,1);
+
+        if (lastTaskTransform == null) //no tasks in list
+        {
+            newTask.transform.position = topOfList;
+        }
+
+        else
+        {
+            //Put new task below last task
+            Vector3 newPosition = new Vector3(
+                lastTaskTransform.localPosition.x, 
+                lastTaskTransform.localPosition.y-taskGap,
+                lastTaskTransform.localPosition.z);
+            Debug.Log("New Position is " + newPosition.ToString());
+            newTask.transform.localPosition = newPosition;
+            Debug.Log("Task put at " + newTask.transform.localPosition.ToString());
+        }
     }
 }
